9470a573fb2763feebe27cefea91064c54bd7c31
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2022-09-06 14:50:44 -0300

    complemento final

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ async function criandoUsuario(){
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(novousuario)
     })
-    .then(res => res.JSON)
+    .then(res => res.json())
     .then(res => console.log(res))
 }
 
 async function pegandoUsuarios() {
     fetch("http://localhost:3000/usuario")
-    .then(res => res.JSON)
+    .then(res => res.json())
     .then(res => {
         const usuarios = res.usuarios;
         usuarios.map(usuarios => {
@@ -29,4 +29,79 @@ async function pegandoUsuarios() {
     })
 
 
+}
+
+async function alterando(){
+    fetch("http://localhost:3000/usuario/6", {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email: "soulindo@gmail.com"})
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+}
+
+async function loginUsuario(){
+    const login = {
+        usuario: "Jonathas",
+        senha: "1234"
+    }
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(login)
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+}
+
+async function usuario(){
+    fetch("http://localhost:3000/usuario")
+    .then(res => res.json())
+    .then(res => {
+        const usuarios = res.usuarios;
+        usuarios.map(usuario => {
+            
+        })
+    })
+}
+
+async function usuario (){
+    fetch("http://localhost:3000/usuario")
+    .then(res => res.json ())
+    .then(res => {
+       const usuarios = res.usuarios;
+       usuarios.map(usuario =>{
+        console.log(usuario)
+        const getId = document.querySelector("#usuarios");
+        enviar(usuarios.cep)
+        const li = document.createElement("li");
+        li.innerHTML = usuario.nome
+        getId.appendChild(li)
+       })
+    })
+}
+
+async function enviar() {
+    const cep = document.getElementById("input-cep");
+    const valorCep = cep.value
+    const adress = await fetch(`https://viacep.com.br/ws/${valorCep}/json/`)
+    const jsonAdress = await adress.json()
+    console.log(jsonAdress)
+    const tableCep = document.getElementById("table-cep");
+    const tr = document.createElement("tr");
+    for (var atributo in jsonAdress) {
+        const td = document.createElement("td");
+        td.innerHTML = jsonAdress[atributo];
+        tr.appendChild(td);
+
+    }
+    tableCep.appendChild(tr);
+
+//Formulário////////////////
+const inputRua = document.getElementById("input-rua");
+inputRua.value = jsonAdress.logradouro;
+
+const inputUf = document.getElementById("input-uf");
+inputUf.value = jsonAdress.uf;
 }
